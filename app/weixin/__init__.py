c44e07c20be4bcab628679d404890de42f4cfe50
stamaimer <stamaimer@gmail.com>
2016-08-26 00:07:55 +0800

    add weixin message manage

--- app/weixin/__init__.py
+++ app/weixin/__init__.py
@@ -57,4 +57,59 @@ def verify():
         current_app.logger.error(sys.exc_info())
 
 
+@weixin.route('/', methods=["POST"])
+def message():
 
+    try:
+
+        data = xmltodict.parse(request.data)["xml"]
+
+        xml = Element("xml")
+
+        ToUserName = SubElement(xml, "ToUserName")
+
+        ToUserName.text = CDATA(data["FromUserName"])
+
+        FromUserName = SubElement(xml, "FromUserName")
+
+        FromUserName.text = CDATA(data["ToUserName"])
+
+        CreateTime = SubElement(xml, "CreateTime")
+
+        CreateTime.text = str(int(time.time()))
+
+        MsgType = SubElement(xml, "MsgType")
+
+        MsgType.text = CDATA("text")
+
+        Content = SubElement(xml, "Content")
+
+        if data["MsgType"] == "event" and data["Event"] == "subscribe":
+
+            Content.text = CDATA(u"感谢关注")
+
+        elif data["MsgType"] == "text" and data["Content"] == u"最新成绩":
+
+            Content.text = CDATA(u"最新成绩")
+
+        elif data["MsgType"] == "text" and data["Content"] == u"所有成绩":
+
+            Content.text = CDATA(u"历史成绩")
+
+        else:
+
+            Content.text = CDATA(u"再说一遍")
+
+    except:
+
+        current_app.logger.error(sys.exc_info())
+
+    finally:
+
+        current_app.logger.debug(tostring(xml, encoding="utf-8", pretty_print=1))
+
+        return tostring(xml, encoding="utf-8")
+
+
+import menu
+import access_token
