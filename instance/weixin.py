1af56c8940cadf582d8ccbc365733a5399c4b22a
stamaimer <stamaimer@gmail.com>
2016-09-04 23:06:29 +0800

    add flask sticker

---
+++ instance/weixin.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+    coin.instance.weixin
+    ~~~~~~~~~~~~~~~~~~~~
+
+    stamaimer 08/23/16
+
+"""
+
+WEIXIN_TOKEN = "szc"
+
+WEIXIN_APP_ID = "wxe5b1a1437e2e999c"
+
+WEIXIN_APP_SECRET = "7bcd7cdd5e3663bfa6d7ee37875ccbd0"
+
+WEIXIN_ENDPOINT = "https://api.weixin.qq.com"
+
+WEIXIN_SUBSCRIBE_RESPONSE = u"感谢关注七宝中学家校互动平台，使用该平台您可以查询孩子的最近一次考试的成绩以及所有成绩。\n" \
+                            u"使用说明：\n" \
+                            u"1、首次使用需要发送'绑定+学号+学生姓名'进行绑定操作(例如：'绑定+1234567890+李明')；\n" \
+                            u"2、绑定之后，查询最近一次的考试成绩需要发送'最新成绩'；\n" \
+                            u"3、绑定之后，查询所有成绩需要发送'所有成绩'；\n" \
+                            u"4、绑定之后，输入'解绑'，进行解绑操作。"
