5c8968fc9bdd5ea1b6971ef80e94c3bf3ecf1397
KlarY <klar0928@gmail.com>
2016-09-12 02:30:22 +0800

    fx

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -10,19 +10,19 @@
 """
 
 from datetime import datetime
-from flask import Blueprint, current_app, send_from_directory
+from flask import Blueprint, current_app, send_from_directory,make_response
 from flask import abort, make_response, redirect, render_template, request
 from flask_security import login_required
 from flask_sqlalchemy import get_debug_queries
 from app.model.pip_user import Pip_user
-import hashlib, json
+import hashlib, json, time
 
 main = Blueprint("main", __name__)
 
 
 @main.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html")
+    return render_template("404.html", error=''.join(e.description.args)), 404
 
 
 @main.before_app_first_request
@@ -32,6 +32,8 @@ def before_app_first_request():
 
 @main.before_app_request
 def before_app_request():
+    # if request.endpoint != 'main.profile' or request.endpoint != 'main.cv' or request.endpoint != 'main.news' or request.endpoint != 'main.podcast' or request.endpoint != 'main.pip' or request.endpoint != 'main.portfolio':
+
     pass
 
 
@@ -90,8 +92,11 @@ def auth():
     try:
         user = Pip_user.query.filter_by(access_code=request.args.get('access-code')).first()
 
-        return render_template("auth.html", user=user.to_json())
+        resp = make_response(render_template("auth.html", user=user.to_json()))
 
+        resp.set_cookie(key='ac', value=hashlib.md5(user.access_code).hexdigest(), expires=time.time() + 60 * 60)
+
+        return resp
     except:
 
         return render_template("auth.html")
@@ -111,13 +116,15 @@ def profile(user_id):
 
             return render_template("profile.html", user=user, owner=True)
 
-        else:
+        elif hashlib.md5(user.access_code).hexdigest() == request.cookies.get('ac'):
 
             return render_template("profile.html", user=user, owner=False)
 
+        else:
+            raise Exception('You have no access to view this profile')
     except Exception as e:
 
-        abort(404)
+        abort(404,e)
 
 
 @main.route('/invite')
